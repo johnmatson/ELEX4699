c81e78376cc4881ba3087541357837ce28fbf79f
John Matson <john@matsonengineering.com>
2020-05-13 17:58:33 -0700

    v0.5 servoMotor class created dcMotor class fixed

--- robot.py
+++ robot.py
@@ -31,10 +31,9 @@ class dcMotor:
 
         self.PWM = GPIO.PWM(PWM, PWMFREQ) # create PWM object
         self.PWM.start(0) # initialize PWM
-        self.motor = (IN1,IN2,PWM) # create motor object
+        self.motor = (IN1,IN2,self.PWM) # create motor object
 
         self.PWMSLEEP = PWMSLEEP
-
 
     def fwd(self):
         GPIO.output(self.motor[0], GPIO.LOW)
@@ -55,98 +54,37 @@ class dcMotor:
             for DC in range(initDC-finalDC):
                 self.motor[2].ChangeDutyCycle(initDC - DC)
                 time.sleep(self.PWMSLEEP)
-    
-    def __del__(self):
-        GPIO.cleanup()
 
 
-class piMotor:
+class servoMotor:
 
-    # pins constants
-    AIN1 = 11
-    AIN2 = 13
-    APWM = 15
-    BIN1 = 29
-    BIN2 = 31
-    BPWM = 33
-    SERVOPWM = 32
+    def __init__(self, PWM, PWMFREQ=50, PWMSLEEP=100e-6):
+        GPIO.setmode(GPIO.BOARD) # define pin numbering scheme
+        GPIO.setup(PWM, GPIO.OUT) # setup servo pin
 
-    #  misc constants
-    PWMFREQ = 10e3
-    PWMSLEEP = 100e-6
+        self.PWM = GPIO.PWM(PWM, PWMFREQ) # create PWM object
+        self.PWM.start(0) # initialize PWM
 
-    # create PWM pin variables
-    leftPWM = GPIO.PWM(APWM, PWMFREQ)
-    rightPWM = GPIO.PWM(BPWM, PWMFREQ)
-    servoPWM = GPIO.PWM(SERVOPWM, PWMFREQ)
+        self.PWMFREQ = PWMFREQ
+        self.PWMSLEEP = PWMSLEEP
 
-    # create motor "objects"
-    leftMotor = (AIN1,AIN2,leftPWM)
-    rightMotor = (BIN1,BIN2,rightPWM)
+    def setPos(self, val): # val should be between 0 & 180
+        # convert angle in degrees to duty cycle in pencentage for a 1-2ms pulse
+        dutyCycle = self.PWMFREQ*(val+180)/1800
+        self.PWM.ChangeDutyCycle(dutyCycle)
 
-    motorDict = {'left':leftMotor, 'right':rightMotor}
+    # def inc(self, initDC=0, finalDC=100):
+    #     if initDC < finalDC:
+    #         for DC in range(finalDC-initDC):
+    #             self.PWM.ChangeDutyCycle(initDC + DC)
+    #             time.sleep(self.PWMSLEEP)
 
-    def __init__(self):
-        # define pin numbering scheme
-        GPIO.setmode(GPIO.BOARD)
+    # def dec(self, initDC=100, finalDC=0):
+    #     if initDC > finalDC:
+    #         for DC in range(initDC-finalDC):
+    #             self.PWM.ChangeDutyCycle(initDC - DC)
+    #             time.sleep(self.PWMSLEEP)
 
-        # setup left motor pins
-        GPIO.setup(self.AIN1, GPIO.OUT)
-        GPIO.setup(self.AIN2, GPIO.OUT)
-        GPIO.setup(self.APWM, GPIO.OUT)
-
-        # setup right motor pins
-        GPIO.setup(self.BIN1, GPIO.OUT)
-        GPIO.setup(self.BIN2, GPIO.OUT)
-        GPIO.setup(self.BPWM, GPIO.OUT)
-
-        # setup servo motor pins
-        GPIO.setup(self.SERVOPWM, GPIO.OUT)
-
-    def fwd(self, motorDes):
-        motor = self.motorDict[motorDes]
-        GPIO.output(motor[0], GPIO.LOW)
-        GPIO.output(motor[1], GPIO.HIGH)
-
-    def rev(self, motorDes):
-        motor = self.motorDict[motorDes]
-        GPIO.output(motor[0], GPIO.HIGH)
-        GPIO.output(motor[1], GPIO.LOW)
-
-    def init(self, motorDes):
-        motor = self.motorDict[motorDes]
-        motor[2].start(0)
-
-    def increase(self, motorDes, initDC=0, finalDC=100):
-        motor = self.motorDict[motorDes]
-        if initDC < finalDC:
-            for DC in range(finalDC-initDC):
-                motor[2].ChangeDutyCycle(initDC + DC)
-                time.sleep(self.PWMSLEEP)
-
-    def decrease(self, motorDes, initDC=100, finalDC=0):
-        motor = self.motorDict[motorDes]
-        if initDC > finalDC:
-            for DC in range(initDC-finalDC):
-                motor[2].ChangeDutyCycle(initDC - DC)
-                time.sleep(self.PWMSLEEP)
-    
-    def __del__(self):
-        GPIO.cleanup()
-
-
-
-# motor = piMotor()
-# motor.init('left')
-# motor.init('right')
-# motor.fwd('left')
-# motor.fwd('right')
-# motor.increase('left')
-# time.sleep(1)
-# motor.increase('right')
-# time.sleep(3)
-# motor.decrease('left')
-# motor.decrease('right')
 
 
 
@@ -161,6 +99,7 @@ SERVOPWM = 32
 
 leftMotor = dcMotor(AIN1, AIN2, APWM)
 rightMotor = dcMotor(BIN1, BIN2, BPWM)
+servoMotor = servoMotor(SERVOPWM)
 
 leftMotor.fwd()
 rightMotor.fwd()
@@ -171,3 +110,14 @@ rightMotor.inc()
 time.sleep(1)
 leftMotor.dec()
 rightMotor.dec()
+
+del leftMotor
+del rightMotor
+
+servoMotor.setPos(0)
+time.sleep(1)
+servoMotor.setPos(90)
+time.sleep(1)
+servoMotor.setPos(180)
+
+del servoMotor
