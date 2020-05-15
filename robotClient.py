d6be3d786fa4947e44eada928df31871560b1781
John Matson <john@matsonengineering.com>
2020-05-14 18:40:02 -0700

    v1.3 client/server video opperational

--- robotClient.py
+++ robotClient.py
@@ -1,29 +1,24 @@
-# import socket
-# import time
-# import picamera
+import socket
+import subprocess
 
-# # Connect a client socket to my_server:8000 (change my_server to the
-# # hostname of your server)
-# client_socket = socket.socket()
-# client_socket.connect(('my_server', 8000))
+# Connect a client socket to my_server:8000 (change my_server to the
+# hostname of your server)
+client_socket = socket.socket()
+client_socket.connect(('192.168.0.27', 8000))
 
-# # Make a file-like object out of the connection
-# connection = client_socket.makefile('wb')
-# try:
-#     camera = picamera.PiCamera()
-#     camera.resolution = (640, 480)
-#     camera.framerate = 24
-#     camera.vflip = True
-#     # Start a preview and let the camera warm up for 2 seconds
-#     camera.start_preview()
-#     time.sleep(2)
-#     # Start recording, sending the output to the connection for 60
-#     # seconds, then stop
-#     camera.start_recording(connection, format='h264')
-#     camera.wait_recording(60)
-#     camera.stop_recording()
-# finally:
-#     connection.close()
-#     client_socket.close()
-
-/Applications/VLC.app/Contents/MacOS/VLC tcp/h264://my_pi_address:8000/
+# Make a file-like object out of the connection
+connection = client_socket.makefile('rb')
+try:
+    # Run a viewer with an appropriate command line
+    cmdline = ['/Applications/VLC.app/Contents/MacOS/VLC', '--demux', 'h264', '-']
+    player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+    data = ' '
+    while data:
+        # Repeatedly read 1k of data from the connection and write it to
+        # the media player's stdin
+        data = connection.read(1024)
+        player.stdin.write(data)
+finally:
+    connection.close()
+    client_socket.close()
+    player.terminate()
