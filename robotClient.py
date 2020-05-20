56cb69caba3252df2c0158a4ae5269c847f8f793
John Matson <john@matsonengineering.com>
2020-05-20 14:57:24 -0700

    v2.0 robotClient/robotServer converted to asyncio echo server

--- robotClient.py
+++ robotClient.py
@@ -1,25 +1,43 @@
-import socket
-import subprocess
+# import socket
+# import subprocess
 
-# Connect a client socket to my_server:8000 (change my_server to the
-# hostname of your server)
-client_socket = socket.socket()
-client_socket.connect(('192.168.0.27', 4699))
+# # Connect a client socket to my_server:8000 (change my_server to the
+# # hostname of your server)
+# client_socket = socket.socket()
+# client_socket.connect(('192.168.0.27', 4699))
 
-# Make a file-like object out of the connection
-vidStream = client_socket.makefile('rb')
-# cmdStream = client_socket.makefile('wb')
-try:
-    # Run a viewer with an appropriate command line
-    cmdline = ['/Applications/VLC.app/Contents/MacOS/VLC', '--demux', 'h264', '-']
-    player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
-    data = ' '
-    while data:
-        # Repeatedly read 1k of data from the connection and write it to
-        # the media player's stdin
-        data = vidStream.read(1024)
-        player.stdin.write(data)
-finally:
-    vidStream.close()
-    client_socket.close()
-    player.terminate()
+# # Make a file-like object out of the connection
+# vidStream = client_socket.makefile('rb')
+# # cmdStream = client_socket.makefile('wb')
+# try:
+#     # Run a viewer with an appropriate command line
+#     cmdline = ['/Applications/VLC.app/Contents/MacOS/VLC', '--demux', 'h264', '-']
+#     player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+#     data = ' '
+#     while data:
+#         # Repeatedly read 1k of data from the connection and write it to
+#         # the media player's stdin
+#         data = vidStream.read(1024)
+#         player.stdin.write(data)
+# finally:
+#     vidStream.close()
+#     client_socket.close()
+#     player.terminate()
+
+
+import asyncio
+
+async def tcp_echo_client(message):
+    reader, writer = await asyncio.open_connection(
+        '192.168.0.27', 8888)
+
+    print(f'Send: {message!r}')
+    writer.write(message.encode())
+
+    data = await reader.read(100)
+    print(f'Received: {data.decode()!r}')
+
+    print('Close the connection')
+    writer.close()
+
+asyncio.run(tcp_echo_client('Hello World!'))
--- robotServer.py
+++ robotServer.py
@@ -1,27 +1,57 @@
-import socket
-import time
-import picamera
+# import socket
+# import time
+# import picamera
 
-camera = picamera.PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 24
-camera.vflip = True
+# camera = picamera.PiCamera()
+# camera.resolution = (640, 480)
+# camera.framerate = 24
+# camera.vflip = True
+# camera.hflip = True
 
-server_socket = socket.socket()
-server_socket.bind(('0.0.0.0', 4699))
-server_socket.listen(0)
+# server_socket = socket.socket()
+# server_socket.bind(('0.0.0.0', 4699))
+# server_socket.listen(0)
 
-# Accept a single connection and make a file-like object out of it
-vidStream = server_socket.accept()[0].makefile('wb')
-# cmdStream = server_socket.accept()[0].makefile('rb')
-try:
-    camera.start_recording(vidStream, format='h264')
-    exitKey = False
-    while not exitKey:
-        cmd = input('enter e to exit: ')
-        if cmd is 'e':
-            exitKey = True
-    camera.stop_recording()
-finally:
-    vidStream.close()
-    server_socket.close()
+# # Accept a single connection and make a file-like object out of it
+# vidStream = server_socket.accept()[0].makefile('wb')
+# # cmdStream = server_socket.accept()[0].makefile('rb')
+# try:
+#     camera.start_recording(vidStream, format='h264')
+#     exitKey = False
+#     while not exitKey:
+#         cmd = input('enter e to exit: ')
+#         if cmd is 'e':
+#             exitKey = True
+#     camera.stop_recording()
+# finally:
+#     vidStream.close()
+#     server_socket.close()
+
+
+import asyncio
+
+async def handle_echo(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+
+    print(f"Received {message!r} from {addr!r}")
+
+    print(f"Send: {message!r}")
+    writer.write(data)
+    await writer.drain()
+
+    print("Close the connection")
+    writer.close()
+
+async def main():
+    server = await asyncio.start_server(
+        handle_echo, '0.0.0.0', 8888)
+
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
+
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(main())
